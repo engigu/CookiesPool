e37a20a9a8472168d7e3bb907d8d94319a0ac035
EngiGu <sayheya@qq.com>
2020-01-12 22:26:34 -0800

    update

--- vue/static/main.js
+++ vue/static/main.js
@@ -13,35 +13,55 @@ var default_site = get_site_from_url()
 
 Vue.component("listpage", {
     template: `
+<div id="show_page">
     <table v-show="isShow">
-    listpage
-    {{ query_site }} 站点信息
-    {{ query_site }}
-    {{ get_cookies }}  <!-- 更改数据 -->
-    {{ total }}
-    <tr>
-        <td v-for="col in col_list">{{ col }}</td>
-    </tr>
+        {{ get_site_info }}   <!-- 更改数据 -->
+        站点信息
+        <tr v-for="(site_value,site_key, index) in site_infos">
+            <td>{{ site_key }}</td>
+            <td>
+                <input :value="site_value" v-show="site_key!='modified'"></input>
+                <p v-show="site_key==='modified'">{{ site_value }}</p>
+            </td>
+            <td>    
+                <button v-if="site_key==='modified'">修改</button>
+                <button v-if="site_key==='modified'">添加</button>
+            </td>
+        </tr>
+    </table>
 
-    <tr>
-        <td><input v-model="to_add_cookies_name" placeholder="可留空"></input></td>
-        <td><input v-model="to_add_cookies"></input></td>
-        <td>-</td>
-        <td>-</td>
-        <button @click="addCookies(query_site)">添加</button>
+    <table v-show="isShow">
+        {{ get_cookies }}  <!-- 更改数据 -->
+        <tr>
+            <td v-for="col in col_list">{{ col }}</td>
+        </tr>
 
-    </tr>
+        <tr>
+            <td><input v-model="to_add_cookies_name" placeholder="可留空"></input></td>
+            <td><input v-model="to_add_cookies"></input></td>
+            <td>-</td>
+            <td>-</td>
+            <button @click="addCookies(query_site)">添加</button>
 
-    <tr v-for="(item, index) in list_data">
-        <td><input :value="item.cookies_name"  @input="inputItem('cookies_name', $event.target.value, index)"></input></td>
-        <td><input :value="item.cookies"  @input="inputItem('cookies', $event.target.value, index)"></input></td>
-        <td> {{ item.modified }} </td>
-        <td v-text="item.status?'不可用':'可用'"></td>
-        <button @click="updateItem(index)">修改</button>
-        <button @click="deleteCookies(item.id, index)">删除</button>
-    </tr>
+        </tr>
 
-</table>`,
+        <tr v-for="(item, index) in list_data">
+            <td><input :value="item.cookies_name"  @input="inputItem('cookies_name', $event.target.value, index)"></input></td>
+            <td><input :value="item.cookies"  @input="inputItem('cookies', $event.target.value, index)"></input></td>
+            <td> {{ item.modified }} </td>
+            <td v-text="item.status?'不可用':'可用'"></td>
+            <button @click="updateItem(index)">修改</button>
+            <button @click="deleteCookies(item.id, index)">删除</button>
+        </tr>
+
+        <button>上一页</button>
+        <input></input>
+        <button>下一页</button><p>共{{ total }}条</p>
+
+    </table>
+</div>
+`
+    ,
     data() {
         return {
             "list_data": "",
@@ -51,6 +71,7 @@ Vue.component("listpage", {
             "total": "",
             "to_add_cookies_name": "",
             "to_add_cookies": "",
+            "site_infos": "",
         }
     },
     props: ["query_site"],
@@ -65,6 +86,19 @@ Vue.component("listpage", {
                 })
             }
         },
+        get_site_info() {
+            if (this.query_site != '') {
+                axios.get('/cookies_this_site?site=' + this.query_site).then(response => (
+                    site = response.data.site,
+                    delete site.id,
+                    delete site.created,
+                    delete site.status,
+                    this.site_infos = site
+                )).catch(function (err) {
+                    console.log(err)
+                })
+            }
+        },
         isShow() {
             return this.query_site != ''
         }
@@ -72,13 +106,16 @@ Vue.component("listpage", {
     methods: {
         deleteCookies(cookies_id, index) {
             axios.delete('/cookies?cookies_id=' + cookies_id).then(
-                response => (this.list_data.splice(index, 1), alert('删除成功！'))
+                response => (
+                    this.list_data.splice(index, 1),
+                    alert('删除成功！')
+                )
             ).catch(response => (''))
         },
         inputItem(name, input_item, index) {
             this.$set(this.list_data[index], name, input_item)
             this.list_data[index].has_editied = true
-            console.log(input_item, index, this.list_data[index].has_editied)
+            // console.log(input_item, index, this.list_data[index].has_editied)
         },
         updateItem(index) {
             if (this.list_data[index].has_editied === true) {
@@ -87,7 +124,9 @@ Vue.component("listpage", {
                 formData.append('cookies', this.list_data[index].cookies);
                 formData.append('cookies_name', this.list_data[index].cookies_name);
                 axios.put('/cookies', formData).then(
-                    response => (alert('修改成功！'))
+                    response => (
+                        alert('修改成功！')
+                    )
                 ).catch(response => (''))
             } else {
                 alert('没有改动，不需要更改！')
@@ -102,7 +141,9 @@ Vue.component("listpage", {
                 formData.append('cookies', this.to_add_cookies);
                 formData.append('cookies_name', this.to_add_cookies_name);
                 axios.post('/cookies', formData).then(
-                    response => (alert('添加成功！'))
+                    response => (
+                        alert('添加成功！')
+                    )
                 ).catch(response => (''))
             }
         }
